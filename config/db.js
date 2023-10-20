89ec40ebd5ad1791923125e976d2c0a9db4c735d
gawdesakshi560 <gawdesakshi560@gmail.com>
2023-10-20 12:46:30 +0530

    hello world

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
-require("mongoose").connect("mongodb+srv://rrohu786:hwBdyozEal5zBNCB@cluster0.zsiavfo.mongodb.net/?retryWrites=true&w=majority")
-
+require("mongoose").connect("mongodb+srv://rrohu786:hwBdyozEal5zBNCB@cluster0.zsiavfo.mongodb.net/?retryWrites=true&w=majoritye:")
+//mongodb+srv://rrohu786:hwBdyozEal5zBNCB@cluster0.zsiavfo.mongodb.net/?retryWrites=true&w=majority
 .then(()=>{
   console.log("DATA BASE CONNECTED");
 })
